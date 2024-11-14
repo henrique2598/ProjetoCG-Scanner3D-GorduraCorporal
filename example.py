8d2000f9b59206953146d18e2df2ebd99742a922
henrique2598 <henriquevicente25@hotmail.com>
2024-11-14 00:14:27 -0300

    Add Spinners

--- example.py
+++ example.py
@@ -2,22 +2,23 @@ import kivy
 from panda3d_kivy.app import App
 from kivy.uix.widget import Widget
 
-from kivy.core.window           import Window
+from plyer import filechooser
 
 kivy.require('2.3.0')
-
-from kivy.uix.popup import Popup
 
 
 
 # Declara a Tela do Software
 class TelaSoftware(Widget):
     def select_file(self):
-        from plyer import filechooser
         filechooser.open_file(on_selection = self.selected)
     
     def selected(self, selection):
         print(selection[0])
+
+    def spinner_clicked(self, value):
+        pass
+
 
 class Example(App):
     def build(self):
